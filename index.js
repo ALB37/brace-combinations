7d971031aa949599b1c60654f705cc2f18b85a2e
Andrew Bloom <blooma116@gmail.com>
2018-01-30 23:00:09 -0800

    working on valid brace problem

---
+++ index.js
@@ -0,0 +1,54 @@
+// Problem Statement
+// Implement an algorithm to print all valid(i.e.properly opened and closed) combinations of n pairs of braces.
+
+// Example:
+// Input: 3
+
+// Output:
+// {{{}}}
+// {{}{}}
+// {{}}{}
+// {}{{}}
+// {}{}{}
+
+
+const validCombinations = num => {
+  let open = [];
+  let closed = [];
+  for (let i = 0; i < num; i++) {
+    open.push('{');
+  }
+  for (let i = 0; i < num; i++) {
+    closed.push('}');
+  }
+  const combinations = new Set();
+  const _close = (open, closed, str) => {
+    let newStr;
+    while (closed.length > open.length){
+      newStr = str + closed.pop();
+      _open(open, closed, newStr);
+      _close(open, closed, newStr);
+    }
+  };
+  const _open = (open, closed, str) => {
+    let newStr;
+    while (open.length){
+      console.log(open);
+      newStr = str + open.pop();
+      _close(open, closed, newStr);
+      _open(open, closed, newStr);
+    }
+    if (closed.length){
+      newStr = str + closed.join('');
+    }
+    if (newStr)
+      combinations.add(newStr);
+  };
+  _open(open, closed, '');
+  return combinations;
+};
+
+
+
+
+console.log(validCombinations(3));
