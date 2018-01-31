cbcec25cde321807b1c923cb08089e4df7941b18
Andrew Bloom <blooma116@gmail.com>
2018-01-30 23:45:05 -0800

    code working and optimized

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # brace-combinations
+
+## Problem Statement
+Implement an algorithm to print all valid (i.e.properly opened and closed) combinations of n pairs of braces.
+
+### Example:
+```javascript
+Input: 3
+
+Output:
+{{{}}}
+{{}{}}
+{{}}{}
+{}{{}}
+{}{}{}
+```
--- index.js
+++ index.js
@@ -1,53 +1,23 @@
-// Problem Statement
-// Implement an algorithm to print all valid(i.e.properly opened and closed) combinations of n pairs of braces.
-
-// Example:
-// Input: 3
-
-// Output:
-// {{{}}}
-// {{}{}}
-// {{}}{}
-// {}{{}}
-// {}{}{}
-
-
 const validCombinations = num => {
-  let open = [];
-  let closed = [];
-  for (let i = 0; i < num; i++) {
-    open.push('{');
-  }
-  for (let i = 0; i < num; i++) {
-    closed.push('}');
-  }
-  const combinations = new Set();
-  const _close = (iter, open, closed, str) => {
-    let newStr;
-    while (closed.length > open.length){
-      newStr = str + closed.pop();
-      _open(open, closed, newStr);
-      _close(open, closed, newStr);
+
+  let combinations = [];
+
+  const _helper = (index, openNum, closedNum, string) => {
+    if (index < 2 * num){
+      if (openNum > 0){
+        _helper(index + 1, openNum - 1, closedNum, string + '{');
+      }
+      if (openNum < closedNum && closedNum > 0){
+        _helper(index + 1, openNum, closedNum - 1, string + '}');
+      }
+    } else {
+      combinations.push(string);
     }
+
   };
-  const _open = (iter, open, closed, str) => {
-    let newStr;
-    while (open.length){
-      console.log(open);
-      newStr = str + open.pop();
-      _open(open, closed, newStr);
-      _close(open, closed, newStr);
-    }
-    if (closed.length){
-      newStr = str + closed.join('');
-    }
-    if (newStr)
-      combinations.add(newStr);
-  };
-  _open(open, closed, '');
+  _helper(0, num, num, '');
   return combinations;
 };
-
 
 
 
